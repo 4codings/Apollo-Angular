39fc690b79ac3a002995a624cea80dae8e82373b
4codings <proudofdeveloper@gmail.com>
2018-06-16 23:52:35 -0400

    Reach out to Apollo

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloModule, Apollo } from 'apollo-angular';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +13,26 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpLinkModule,
+    ApolloModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    const uri = `http://${environment.apiHost}:${environment.apiPort}`;
+
+    apollo.create({
+      link: httpLink.create({
+        uri: uri
+      }),
+      cache: new InMemoryCache()
+    })
+  }
+}
