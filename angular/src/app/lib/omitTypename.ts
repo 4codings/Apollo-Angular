cbc3edd24b654d203ff3b33a14b369c91da7fcda
4codings <proudofdeveloper@gmail.com>
2018-07-22 00:27:48 -0400

    Adding comment

--- angular/src/app/lib/omitTypename.ts
+++ angular/src/app/lib/omitTypename.ts
@@ -1,6 +1,10 @@
 const omitTypenameKey = (key, value) => (key === '__typename' ? undefined : value)
 
-export default function omitTypename(payload) {
+/**
+ * Hack to remove __typename from object while preserving type
+ * https://github.com/apollographql/apollo-client/issues/1913#issuecomment-393721604
+ */
+export default function omitTypename<T>(payload: T): T {
   return JSON.parse(JSON.stringify(payload), omitTypenameKey)
 }
 
